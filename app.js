adde1ae9f3c03948948e37cd0c3b6291d050493e
Ruma <th3rum2@gmail.com>
2022-01-14 21:15:52 -0500

    le agrege otros botones

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
-// Inicio el contador en 0
 let count = 0;
 
-//Llamo mis propiedades del html (valor y los botones)
 const value = document.querySelector('#value');
 const btns = document.querySelectorAll('.btn');
 
-//Aplico el metodo forEach ya que los 3 botones contienen la misma clase
-btns.forEach((btn)=>{
-    btn.addEventListener('click',(e)=>{ //Acada elemento le agrego el evento click
-        const styles = e.currentTarget.classList; // Creo una constante para que cuando le de click a cada uno revise su clase
-        styles.contains('decrease') ? count-- : count; // Si mi variable style con tiene la clase 'Decrease' ocurrira esto
-        styles.contains('increase') ? count++ : count; //
-        styles.contains('reset') ? count = 0 :  count; //
 
-        value.textContent = count; // Cambio el valor en el documento de acuerdo al resultado del contador
+// Utilizo el metodo forEach
+
+btns.forEach((btn)=>{
+    btn.addEventListener('click', (e)=>{
+        const styles = e.currentTarget.classList;
+        styles.contains('decrease') ? count-- : count;
+        styles.contains('decrease2') ? count-= 2 : count;
+        styles.contains('increase') ? count++ : count;
+        styles.contains('increase2') ? count+= 2 : count;
+        styles.contains('reset') ? count = 0  : count;
+        styles.contains('random') ? count = numAleatorio()  : count;
+
+        value.textContent = count;
 
         if(count > 0) {
             value.style.color = 'green'
-        } else if ( count < 0) {
-            value.style.color = 'red'
+        } else if ( count < 0 ) {
+            value.style.color = 'red';
         } else {
             value.style.color = 'black'
         }
+
     })
 })
+
+function numAleatorio(){
+    
+    return Math.floor(Math.random()*100)
+}
+
+
